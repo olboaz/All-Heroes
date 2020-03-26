d31f6deee5e46ecbce5cd06c354cae4541750601
patrice jaszczynski <ppastg92@free.fr>
2020-03-26 23:20:16 +0100

    Fix Add validations

--- app/models/heroe.rb
+++ app/models/heroe.rb
@@ -2,6 +2,6 @@ class Heroe < ApplicationRecord
   belongs_to :publisher
   has_many :users, through: :users_heroes
 
-  validates :name, :description, :address, :image_hero, :height, :weight presence: true
+  validates :name, :description, :address, :image_hero, :height, :weight,  presence: true
 
 end
