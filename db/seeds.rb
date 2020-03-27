b19edaade9fcb5d11aef3be2c5849362fdd89d90
patrice jaszczynski <ppastg92@free.fr>
2020-03-27 01:04:18 +0100

    Add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Heroe.destroy_all
+
+
+require 'json'
+require 'open-uri'
 require 'faker'
 
-puts 'Creating 10 fake heroes...'
-10.times do
-  heroe = Heroe.new(
-    name:    Faker::Superhero.name,
-    address: Faker::Address.full_address,
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude
-  )
-  heroe.save!
-end
+my_api_key = ENV['HERO_API_KEY']
+
+(1..10).each { |i|
+url = "https://superheroapi.com/api/#{my_api_key}/#{i}"
+hero_serialized = open(url).read
+hero = JSON.parse(hero_serialized)
+
+publisher = Publisher.new(name: hero['biography']['publisher'])
+publisher.save!
+
+heroe = Heroe.new(
+  name: hero['name'],
+  description: hero['name'],
+  address: Faker::Address.full_address,
+  image_hero: hero['image']['url'],
+  height: hero['appearance']['height'][1],
+  weight: hero['appearance']['weight'][1],
+  publisher_id: i
+)
+heroe.save!
+}
+
+
+# puts 'Creating 10 fake heroes...'
+# 10.times do
+#   heroe = Heroe.new(
+#     name:    Faker::Superhero.name,
+#     address: Faker::Address.full_address,
+#     latitude: Faker::Address.latitude,
+#     longitude: Faker::Address.longitude
+#   )
+#   heroe.save!
+# end
 puts 'Finished!'
