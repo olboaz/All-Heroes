c6b05e585a1c855d76ede32ff3c06aebfe5a87b3
patrice jaszczynski <ppastg92@free.fr>
2020-04-07 10:05:05 +0200

    Add Navigation control on map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -43,6 +43,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    map.addControl(new mapboxgl.NavigationControl());
   }
 };
 
