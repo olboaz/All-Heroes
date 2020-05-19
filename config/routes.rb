4c05f02e8ada347252a7bd8f27546ed5cda9b3cb
patrice jaszczynski <ppastg92@free.fr>
2020-05-19 12:09:16 +0200

    Add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :heroes
+  resources :heroes do
+    resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
+
   resources :users, except: [ :index, :destroy ]
   resources :publisers, only: [:new, :create]
   resources :contacts, only: [:new, :create]
